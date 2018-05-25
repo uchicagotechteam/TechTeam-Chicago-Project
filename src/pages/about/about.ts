79b81105009300af0cd72ad8b5151534e6669e51
Celiajm <celiajm@uchicago.edu>
2018-05-25 09:30:06 -0500

    about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,72 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public buttonClicked: boolean = false; //Whatever you want to initialise it as
 
+  public onButtonClick() {
 
-  searchQuery: string = '';
-  items: string[];
+    this.buttonClicked = !this.buttonClicked;
+  }
 
   constructor(public navCtrl: NavController) {
-    // this.initializeItems();
+
   }
+
 }
 
-window.onload = nodeList; // loads the nodeList upon window load
 
-
-function togglePlotSum() { // shows or hides the summaries for each play upon mouse click
-    var playid = this.id;
-
-    var play1 = document.getElementById("romeo_plot");
-    var play2 = document.getElementById("hamlet_plot");
-    var play3 = document.getElementById("much_plot");
-    var play4 = document.getElementById("temp_plot");
-
-    switch (playid) { // switches on each plot id and makes summary visible if hidden and hidden if visible
-            case "romeo":
-                if (play1.className === "hidden") {
-                    play1.className = "show";
-                }
-                else if (play1.className === "show") {
-                    play1.className = "hidden";
-                }
-                break;
-
-            case "hamlet":
-                if (play2.className === "hidden") {
-                    play2.className = "show";
-                }
-                else if (play2.className === "show") {
-                    play2.className = "hidden";
-                }
-                break;
-
-            case "much":
-                if (play3.className === "hidden") {
-                    play3.className = "show";
-                }
-                else if (play3.className === "show") {
-                    play3.className = "hidden";
-                }
-            break;
-
-            case "temp":
-                if (play4.className === "hidden") {
-                    play4.className = "show";
-                }
-                else if (play4.className === "show") {
-                    play4.className = "hidden";
-                }
-            break;
-        }
-    }
-
-function nodeList() {  // creates nodeList and event listeners
-    var header = document.getElementsByClassName("playheader"); // gets elements with class "playheader"
-    for (var i = 0; i < header.length; i++) {
-        var play = header[i];
-
-        play.addEventListener("click", togglePlotSum, false);
-    }
-}
 
